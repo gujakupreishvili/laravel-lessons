94182b1869a74d760119f13d5393d46bb7eeced6
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-11-25 17:09:34 +0400

    leasson 9 meet Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,29 +1,9 @@
 <?php
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-  public static function all(): array {
-      return [
-          [    
-              "id" => 1,
-              "title" =>"Directro",
-              "salary" =>"$50,000"
-          ],
-          [ 
-              "id" => 2,
-              "title" =>"Programer",
-              "salary" =>"$20,000"
-          ],
-          [
-              "id" => 3,
-              "title" =>"Teacher",
-              "salary" =>"$30,000"
-          ]
-      ];         
-  }
-  public static function find(int $id): array{
-    $job = Arr::first(static ::all() , fn($job) =>$job['id'] == $id);
-    if(!$job)
-      abort(404);
-  }
+
+class Job extends Model  {
+  protected $table = 'job_listings';
+  protected $fillable = ['title', 'salary'];
 }
